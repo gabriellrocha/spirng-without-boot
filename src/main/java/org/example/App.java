d38199380197e02bfb3136866a9c1c5215c27217
gabriel <gabriell_rsilva@outlook.com>
2024-12-13 10:28:06 -0300

    configuring app without spring boot

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,22 +11,22 @@ public class App
 {
     public static void main( String[] args ) {
 
-        // Como é uma aplicação que não usa Spring Boot, todas as configs é feita pelo dev
+//      Aplicação sem Spring Boot
+//
+//      Cria o IoC container - as config são feitas via XML
 
-        // Cria o IoC container - assim como o Spring Boot, porém, as config devem ser feitas via XML
+                                // Em um APP com Spring Boot ele que é o responsável
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
-//    Lembre-se o Spring cria os beans assim que o app é inicializado  (eager initialization) com escopo singleton
+//     Lembre-se o Spring cria os beans assim que o app é inicializado  (eager initialization) com escopo singleton
 
-        Developer developer = context.getBean(Developer.class);
-        Laptop laptop = (Laptop) context.getBean("lap"); // Obter o Bean pelo id
+        Developer developer = context.getBean(Developer.class); // recupera o Bean criado pelo Spring
+
+        Laptop laptop = (Laptop) context.getBean("lap"); // recupera o Bean pelo id - necessário Cast
 
 
         developer.build();
-//        System.out.println(developer.getAge());
-//        System.out.println(developer.getName());
 //        laptop.compile();
-
 
 
     }
--- src/main/java/org/example/Developer.java
+++ src/main/java/org/example/Developer.java
@@ -5,33 +5,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class Developer {
 
-//    private int age;
-//    private String name;
+      private Computer computer;
 
-    private Laptop laptop;
 
-//    public Developer(int age, String name){
-//        this.age = age;
-//        this.name = name;
+//    // Injection via XML
+//    public Developer(Computer computer) {
+//        this.computer = computer;
 //    }
-
-
-    public Developer(Laptop laptop) {
-        this.laptop = laptop;
-    }
 
 
     public void build() {
         System.out.println("This Developer says: Working on Awesome Project");
-        laptop.compile();
-//        System.out.println("I'm old " + age);
+        computer.compile();
     }
 
-    public Laptop getLaptop() {
-        return laptop;
+    public Computer getComputer() {
+        return computer;
     }
 
-    public void setLaptop(Laptop laptop) {
-        this.laptop = laptop;
+    public void setComputer(Computer computer) {
+        this.computer = computer;
     }
 }
